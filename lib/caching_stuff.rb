5fc15c559707fe983be91aca2e12bed6a5240cd3
Lasse Koskela <lasse.koskela@gmail.com>
2010-09-16 00:55:39 +0300

    Fix overly coarse-grained caching by suffixing the URL query parameters into the cache key along with the request path. This used to prevent, for example, showing a page revision diff because 'wiki/show/PageName' would be served from the cache when requesting 'wiki/show/PageName?mode=diff.

--- lib/caching_stuff.rb
+++ lib/caching_stuff.rb
@@ -8,6 +8,12 @@ module CachingStuff
     module Caching
       module Fragments
         def fragment_cache_key(key)
+          param_tokens = params.keys.sort.collect do |param_name|
+            param_value = params[param_name]
+            param_value = param_value.join('/') if param_value.is_a? Array
+            "#{param_name}/#{param_value}"
+          end
+          key = "#{key}/#{param_tokens.join('/')}"
           ActiveSupport::Cache.expand_cache_key(key.is_a?(Hash) ? url_for(key.merge(:host=>"")).split(":///").last : key.split('/')[1..-1].join('/').gsub(/\?format=.*/,''), :views)
         end
       end
